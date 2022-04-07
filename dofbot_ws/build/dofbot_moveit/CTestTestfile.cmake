dba7fff9499d6d2a5e2524f7d51e8d00ca149427
Miguel Angel Gallardo <miguegallardo16@hotmail.com>
2022-04-07 09:57:56 +0200

    Delete dofbot_ws directory

--- dofbot_ws/build/dofbot_moveit/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dofbot/dofbot_ws/src/dofbot_moveit
-# Build directory: /home/dofbot/dofbot_ws/build/dofbot_moveit
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
