5441883a5f814b252c031e0c9bcdab5c3c418eae
Miguel Angel Gallardo <miguegallardo16@hotmail.com>
2022-09-07 09:47:40 +0200

    Delete Directory Dofbot

--- Dofbot_Arm/dofbot_ws/build/dofbot_config/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dofbot/dofbot_ws/src/dofbot_config
-# Build directory: /home/dofbot/dofbot_ws/build/dofbot_config
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
